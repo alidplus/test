da392dd0c87d3b2d0a595a8bab4a8e093057c0d7
atabakhidden <atabakhidden2000@gmail.com>
2020-09-29 11:59:16 +0330

    atabak

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
     atabak
-=======
-=======
     abbas
->>>>>>> efcadbae9f2078663546a56bb12e05984d315173
     Aidin
->>>>>>> 4dfa6333c96bcf30c84e350c81636fd5cd0ea2b6
     Ali gh
   </div>
 </template>
