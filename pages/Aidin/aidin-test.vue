bec6f12fa2ebeaa9922a9ed50c163c13c320d787
AIDIN\Aidin <Aidinkiany@gmail.com>
2020-10-13 12:54:31 +0330

    99/07/22

--- pages/Aidin/aidin-test.vue
+++ pages/Aidin/aidin-test.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="container">
-    <div class="dropdown">
-      <button
-        id="dropdownMenuButton"
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="true"
-        @mouseover="isOpen = true"
-        @mouseleave="isOpen = false"
-      >
-        Dropdown button
-      </button>
-      <div class="dropdown-menu" :class="{ show: isOpen }" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+    <div class="row">
+      <div class="col border">
+        col
+      </div>
+      <div class="col border">
+        col
+      </div>
+      <div class="w-100" />
+      <div class="col border">
+        col
+      </div>
+      <div class="col border">
+        col
       </div>
     </div>
   </div>
