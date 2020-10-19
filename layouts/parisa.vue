d193ceaff0709535674f97bd6c4e77a0a00f1501
ECM\E.C.M <parisa.sarchami73@gmail.com>
2020-10-18 23:08:26 -0700

    p

---
+++ layouts/parisa.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    خوش آمدید
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
--- pages/parisa/index.vue
+++ pages/parisa/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="container">
-    <img src="/11.jpg" alt="11">
+    <h1 style="font-size:80px">
+      HEAD
+    </h1>
+    <p style="color:red;  font-size:100px">
+      Paraghraph
+    </p>
+    <p> <i> خوش آمدید 2</i></p>
+    <br>
+    <img src="1.jpg" width="200" height="200" alt="عکس مورد نظر در دسترس نیست">
+    <br>
+    <a class="nav-link active" hrf="h7ttps://www.w3schools.com">کلیک کنید</a>
   </div>
 </template>
-
 <script>
-export default {}
+export default {
+  layout: 'parisa'
+}
 </script>
 
 <style>
